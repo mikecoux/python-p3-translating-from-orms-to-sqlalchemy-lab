0861a5c4a986e4da9c43126258e1f3505a9c222b
Michael Marcoux <michaelmarcoux99@gmail.com>
2023-05-08 11:31:48 -0600

    completed

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, Index)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class Dog(Base):
     __tablename__ = 'dogs'
-
+    Index('index_name', 'name')
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+    def __repr__(self):
+        return f'Dog {self.id}: ' \
+        + f'{self.name} ' \
+        + f'{self.breed}'
